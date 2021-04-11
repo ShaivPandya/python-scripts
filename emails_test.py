7ce13facc6a801d9254868b9756abc8131a02e51
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2021-04-11 16:40:33 -0600

    Update emails_test.py

--- emails_test.py
+++ emails_test.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python3
-
 import unittest
 from emails import find_email
 
 class EmailsTest(unittest.TestCase):
-    """test_basic tests a simple case where expected parameters are chosen, such as the first and las name
+    """test_basic tests a simple case where expected parameters are chosen, such as the first and last name
     of a person in the csv file"""
     def test_basic(self):
-        """The script file is the first element of input parameters thorugh command line using argv since
-        we already imported the function find_email from emails.py earlier, we will pass None in the place
-        of the script. Adding to None, we will pass the first name and last name as parameters"""
         testcase = [None, "Blossom", "Gill"]                # First name "Blossom", Last name "Gill"
         expected = "blossom@abc.edu"                        # Blossom Gill's email is 'blossom@abc.edu'
         self.assertEqual(find_email(testcase), expected)
@@ -32,7 +27,7 @@ class EmailsTest(unittest.TestCase):
         expected = "Missing parameters"
         self.assertEqual(find_email(testcase), expected)
     
-    """Tests an integer input"""
+    """Tests an integer input which should return 'Parameters can only be letters'"""
     def test_numbers(self):
         testcase = [None, 5, 6]
         expected = "Parameters can only be letters"
